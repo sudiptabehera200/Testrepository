11e23292d599c68d62f26b0d87cf91afaac82337
TestOrganisation <sudiptabehera100@gmail.com>
2020-07-12 12:24:54 +0530

    upload react

---
+++ testazurecode/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SayHello from './components/SayHello'
+
+function App() {
+  return (
+    <div className="App">
+     <SayHello></SayHello>
+    </div>
+  );
+}
+
+export default App;
---
+++ testazurecode/src/components/SayHello.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react'
+function SayHello()
+{
+    return <h1>Tst data</h1>
+}
+
+
+export default SayHello
